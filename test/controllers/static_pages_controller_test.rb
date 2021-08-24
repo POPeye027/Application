fa68576bf02e626ff6d5536528975a20cde28752
POPeye027 <87687785+POPeye027@users.noreply.github.com>
2021-08-24 12:23:13 +0530

    completed 7.4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,15 +4,18 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get Home" do
     get static_pages_Home_url
     assert_response :success
+    assert_select "title","Ruby on rails sample app"
   end
 
   test "should get About" do
     get static_pages_About_url
     assert_response :success
+    assert_select "title","Ruby on rails sample app"
   end
 
   test "should get Help" do
     get static_pages_Help_url
     assert_response :success
+    assert_select "title","Ruby on rails sample app"
   end
 end
