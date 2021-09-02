793dcb6f7d32fc5e7751d5006a2bcd97e7ca7d7c
POPeye027 <87687785+POPeye027@users.noreply.github.com>
2021-09-02 10:58:41 +0530

    completed 10.2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -33,11 +33,10 @@ class User < ApplicationRecord
     return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
+  
   def forget
     update_attribute(:remember_digest, nil)
   end
-  def session_token
-    remember_digest || remember
-  end
+
 
 end
